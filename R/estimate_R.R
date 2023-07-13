d714e8805c9bb0c43bef4399bf0a905342d09f95
dagniel <dagniel@rand.org>
2023-07-13 09:44:16 -0500

    Allow TMLE to use estimates in R function.

--- R/estimate_R.R
+++ R/estimate_R.R
@@ -1,6 +1,6 @@
-estimate_R <- function(delta_if, delta_s_if) {
-  delta  <- mean(delta_if)
-  delta_s <- mean(delta_s_if)
+estimate_R <- function(delta_if, delta_s_if, delta = NULL, delta_s = NULL) {
+  if (is.null(delta)) delta  <- mean(delta_if)
+  if (is.null(delta_s)) delta_s <- mean(delta_s_if)
   n <- length(delta_if)
 
   sigma_sq <- delta^(-2)*mean(delta_if^2) + delta_s^2*delta^(-4)*mean(delta_s_if^2) -
